9027c472b06cd1594b648b796789a1b3beaf09b2
al-ov73 <klever1987@yandex.ru>
2023-12-19 16:08:03 +0300

    add labels

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -4,6 +4,8 @@ from task_manager.tasks.models import Task
 
 class TaskForm(forms.ModelForm):
 
+
+
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'responsible', 'label']
