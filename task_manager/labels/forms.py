b4bc96d572f5a454adb2e536c20df42ab71fcc46
al-ov73 <klever1987@yandex.ru>
2023-12-19 15:35:11 +0300

    labels update

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -4,7 +4,11 @@ from task_manager.labels.models import Label
 
 
 class LabelForm(forms.ModelForm):
-
+    
+    name = forms.CharField(
+        label='Имя',
+        widget=forms.TextInput(attrs={'class': 'form-input'})
+    )
     class Meta:
         model = Label
         fields = ['name']
