81ebef27e937eaff72ea4924337b6c2e18c4e2af
al-ov73 <klever1987@yandex.ru>
2023-12-20 15:29:54 +0400

    add textarea inside name and description

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -3,7 +3,8 @@ from task_manager.tasks.models import Task
 
 
 class TaskForm(forms.ModelForm):
-
+    name = forms.CharField(widget=forms.Textarea)
+    description = forms.CharField(widget=forms.Textarea)
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'responsible', 'label']
