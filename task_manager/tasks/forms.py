1682dbc4ac85fa2fcd0fe6d5ee75a19dfa959e89
al-ov73 <klever1987@yandex.ru>
2023-12-20 15:35:32 +0400

    label for name/description

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -3,8 +3,14 @@ from task_manager.tasks.models import Task
 
 
 class TaskForm(forms.ModelForm):
-    name = forms.CharField(widget=forms.Textarea)
-    description = forms.CharField(widget=forms.Textarea)
+    # name = forms.CharField(
+    #     label='Имя',
+    #     widget=forms.Textarea
+    # )
+    description = forms.CharField(
+        label='Описание',
+        widget=forms.Textarea
+    )
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'responsible', 'label']
