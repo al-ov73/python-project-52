ddd10c9149af4497fbd08157d90c6c85f8eb460a
al-ov73 <klever1987@yandex.ru>
2023-12-22 12:22:12 +0400

    add testfixtures

--- task_manager/labels/tests.py
+++ task_manager/labels/tests.py
@@ -6,49 +6,52 @@ from task_manager.labels.models import Label
 
 class TestLabels(TestCase):
 
-    def test_is_ok_index(self):
-        credentials = {
-            'username': 'Testuser',
-            'email': 'test@test.ru',
-            'password': 'Testpass123',
-        }
-        User.objects.create_user(**credentials)
-        self.client.post('/login/', credentials, follow=True)
+    fixtures = ["user.json", "label.json"]
 
+    credentials = {
+        'username': 'Testuser',
+        'email': 'test@test.ru',
+        'password': 'Testpass123',
+    }
+
+    label_data = {
+        'name': 'label_name',
+    }
+
+    def test_is_ok_index(self):
+
+        # User.objects.create_user(**credentials)
+        self.client.post('/login/', self.credentials, follow=True)
         response = self.client.get('/labels/')
         self.assertEquals(response.status_code, 200)
 
     def test_create_label(self):
 
-        credentials = {
-            'username': 'Testuser',
-            'email': 'test@test.ru',
-            'password': 'Testpass123',
-        }
-        User.objects.create_user(**credentials)
-        self.client.post('/login/', credentials, follow=True)
-
-        label_data = {
-            'name': 'testlabel',
-        }
-        self.client.post('/labels/create/', label_data)
-        label = Label.objects.get(name=label_data['name'])
+        # credentials = {
+        #     'username': 'Testuser',
+        #     'email': 'test@test.ru',
+        #     'password': 'Testpass123',
+        # }
+        # User.objects.create_user(**credentials)
+        self.client.post('/login/', self.credentials, follow=True)
+        label = Label.objects.get(id=1)
         self.assertIsInstance(label, Label)
 
     def test_delete_label(self):
-        credentials = {
-            'username': 'Testuser',
-            'email': 'test@test.ru',
-            'password': 'Testpass123',
-        }
-        User.objects.create_user(**credentials)
-        self.client.post('/login/', credentials, follow=True)
+        # credentials = {
+        #     'username': 'Testuser',
+        #     'email': 'test@test.ru',
+        #     'password': 'Testpass123',
+        # }
+        # User.objects.create_user(**credentials)
+        self.client.post('/login/', self.credentials, follow=True)
 
-        credentials = {
-            'name': 'testlabel',
-        }
-        self.client.post('/labels/create/', credentials, follow=True)
-        label = Label.objects.get(name=credentials['name'])
-        pk = label.pk
-        self.client.post(f'/labels/{pk}/delete/')
+
+        # self.client.post('/labels/create/', self.label_data, follow=True)
+        label = Label.objects.get(id=1)
+        pk = label.id
+        print(pk)
+        print(label)
+        self.client.post('/labels/1/delete/')
+        print(label)
         self.assertFalse(Label.objects.filter(id=pk))
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+FIXTURE_DIRS = (os.path.join(BASE_DIR, 'task_manager.fixtures'),)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
